9619dfbae81484128b24c358231ebe048b8fb7e1
MoyArt <moyart@gmail.com>
2018-06-20 17:38:58 -0500

    Constructor function defined

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,17 @@
 console.log('Scripts loaded');
+
+function stopWatch(){
+    let startTime, endTime, running, duration = 0;
+    this.start = function(){
+
+    };
+    this.stop = function(){
+
+    };
+    this.reset = function(){
+
+    };
+    Object.defineProperty(this, 'duration', ()=>{
+
+    })
+}
